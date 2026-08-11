0fe33c77c561fd22477a94b0c5ac9792b4f12efb
Dakshta Grover <dakshtagrover21@gmail.com>
2026-08-11 12:11:32 +0530

    Added Spring Security with Swagger authentication

--- StudentAPI/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ StudentAPI/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -8,22 +8,48 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+
+
 @Configuration
 public class SwaggerConfig {
 	
 	@Bean
 	public OpenAPI customOpenAPI() {
-
-	    return new OpenAPI()
-	            .info(new Info()
-	                    .title("Student API")
-	                    .version("1.0")
-	                    .description("Student Management REST API")
-	                    .contact(new Contact()
-	                            .name("Dakshta Grover")
-	                            .email("dakshta@example.com")));
+		
+		return new OpenAPI()
+		        .info(new Info()
+		                .title("Student API")
+		                .version("1.0")
+		                .description("Student Management REST API")
+		                .contact(new Contact()
+		                        .name("Dakshta Grover")
+		                        .email("dakshta@example.com")))
+		        .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+		        .components(new Components()
+		                .addSecuritySchemes("basicAuth",
+		                        new SecurityScheme()
+		                                .type(Type.HTTP)
+		                                .scheme("basic")));
 	}
-
+	
 }
+	
 
-
+//	    return new OpenAPI()
+//	            .info(new Info()
+//	                    .title("Student API")
+//	                    .version("1.0")
+//	                    .description("Student Management REST API")
+//	                    .contact(new Contact()
+//	                            .name("Dakshta Grover")
+//	                            .email("dakshta@example.com")));
+//	}
+//
+//}
+//
+//
